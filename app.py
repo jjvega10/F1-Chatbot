51ddd1d8e2e8adf34454b076d7248633fa575170
Javier Vega <jj.vega@live.com>
2025-09-10 07:59:20 -0600

    Separated team and driver functions

--- app.py
+++ app.py
@@ -17,6 +17,9 @@ from selenium.webdriver.chrome.options import Options
 import time
 import htmltabletomd
 import re
+import pandas as pd
+from io import StringIO
+import htmlmin
 from bs4 import BeautifulSoup
 
 
@@ -49,38 +52,62 @@ def extract_text_from_dynamic_site(url, wait_time=10):
         driver.quit()
 
 
+# Drivers
+url = f"https://www.formula1.com/en/results/2025/drivers"
+soup_og = extract_text_from_dynamic_site(url, wait_time=3)
+html_table = soup_og.find_all('table')[0]
+html_text = str(html_table.prettify())
+html_text = re.sub(" class=\"[^\"]*\"", "", html_text)
+html_text = re.sub("<img[^>]*>", "", html_text)
+html_text = re.sub("<a[^>]*>", "", html_text)
+html_text = re.sub("<span[^>]*>", "", html_text)
+html_text = re.sub("</span[^>]*>", "", html_text)
+html_text = re.sub("<br>", "", html_text)
+html_text = re.sub("<p>", "", html_text)
+html_text = re.sub("\\\n *", "", html_text)
+dr_raw_markdown_table = htmltabletomd.convert_table(html_text)
+drivers = pd.read_html(StringIO(html_text))[0]
+dr_html_table = drivers.set_index('POS.')[['DRIVER', 'TEAM', 'PTS.']].to_html(border='')
+dr_minified = htmlmin.minify(dr_html_table, remove_empty_space=True)
+
+# Teams
+url = f"https://www.formula1.com/en/results/2025/team"
+soup_og = extract_text_from_dynamic_site(url, wait_time=3)
+html_table = soup_og.find_all('table')[0]
+html_text = str(html_table.prettify())
+html_text = re.sub(" class=\"[^\"]*\"", "", html_text)
+html_text = re.sub("<img[^>]*>", "", html_text)
+html_text = re.sub("<a[^>]*>", "", html_text)
+html_text = re.sub("<span[^>]*>", "", html_text)
+html_text = re.sub("</span[^>]*>", "", html_text)
+html_text = re.sub("<br>", "", html_text)
+html_text = re.sub("<p>", "", html_text)
+html_text = re.sub("\\\n *", "", html_text)
+tm_raw_markdown_table = htmltabletomd.convert_table(html_text)
+teams = pd.read_html(StringIO(html_text))[0]
+tm_html_table = teams.set_index('POS.')[['TEAM', 'PTS.']].to_html(border='')
+tm_minified = htmlmin.minify(tm_html_table, remove_empty_space=True)
+
 # ------------------------------
 # Tool: Get F1 standings
 # ------------------------------
 @tool
-def get_f1_driver_standings(
-    standings_type: Annotated[str, "Select 'drivers' or 'team' championship standings."]
-) -> str:
-    """Get the current F1 drivers or teams standings. 
-    Use it to determine the points for each driver in the driver's championship or the points for each team in the teams' championship."""
-    if standings_type=="team":
-        url = f"https://www.formula1.com/en/results/2025/team"
-    else:
-        url = f"https://www.formula1.com/en/results/2025/drivers"
-    soup_og = extract_text_from_dynamic_site(url, wait_time=3)
-    html_table = soup_og.find_all('table')[0]
-    html_text = str(html_table.prettify())
-    html_text = re.sub(" class=\"[^\"]*\"", "", html_text)
-    html_text = re.sub("<img[^>]*>", "", html_text)
-    html_text = re.sub("<a[^>]*>", "", html_text)
-    html_text = re.sub("<span[^>]*>", "", html_text)
-    html_text = re.sub("</span[^>]*>", "", html_text)
-    html_text = re.sub("<br>", "", html_text)
-    html_text = re.sub("<p>", "", html_text)
-    html_text = re.sub("\\\n *", "", html_text)
-    drivers_standings_table = htmltabletomd.convert_table(html_text)
-    return drivers_standings_table
+def get_f1_driver_standings() -> str:
+    """Get the current F1 driver standings. 
+    Use it to determine the points for each driver in the drivers' championship."""
+    return dr_minified
+
+@tool
+def get_f1_team_standings() -> str:
+    """Get the current F1 team standings. 
+    Use it to determine the points for each team in the teams' championship."""
+    return tm_minified
 
 
 # ------------------------------
 # Initialize Toolkit
 # ------------------------------
-toolkit = [get_f1_driver_standings]
+toolkit = [get_f1_driver_standings, get_f1_team_standings]
 
 
 # ------------------------------
@@ -105,7 +132,10 @@ def get_response(user_query, chat_history):
 
     # Define prompt (must include input + agent_scratchpad)
     prompt = ChatPromptTemplate.from_template(
-        """You are an expert in Formula 1. We are in the 2025 season.
+        """You are an expert in Formula 1 that will assist the user with questions regarding the sport.
+The current season is 2025, there have been a few changes in terms of driver line-ups.
+If you need to know which driver belongs to a certain team or which team a driver belongs to, use `get_f1_driver_standings`.
+Use `get_f1_driver_standings` and `get_f1_team_standings` to assist the user.
         
 Conversation so far:
 {input}
